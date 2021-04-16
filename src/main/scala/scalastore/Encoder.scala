a540be904d27a838afefa2cb036e3898edf7e1c6
Pishen Tsai <pishen02@gmail.com>
2021-04-16 17:05:54 +0800

    Key name and build-in Encoders

--- src/main/scala/scalastore/Encoder.scala
+++ src/main/scala/scalastore/Encoder.scala
@@ -1,13 +1,22 @@
 package scalastore
 
+import com.google.cloud.Timestamp
+import com.google.cloud.datastore.BlobValue
 import com.google.cloud.datastore.BooleanValue
+import com.google.cloud.datastore.DoubleValue
 import com.google.cloud.datastore.EntityValue
 import com.google.cloud.datastore.FullEntity
 import com.google.cloud.datastore.IncompleteKey
+import com.google.cloud.datastore.Key
+import com.google.cloud.datastore.KeyValue
+import com.google.cloud.datastore.LatLngValue
+import com.google.cloud.datastore.ListValue
 import com.google.cloud.datastore.LongValue
 import com.google.cloud.datastore.StringValue
+import com.google.cloud.datastore.TimestampValue
 import com.google.cloud.datastore.Value
 import magnolia._
+import scala.jdk.CollectionConverters._
 import scala.language.experimental.macros
 
 trait ValueEncoder[T] {
@@ -21,14 +30,31 @@ object ValueEncoder {
     def encode(t: T) = f(t)
   }
 
-  implicit val stringEncoder = create(StringValue.of)
-  implicit val longEncoder = create(LongValue.of)
-  implicit val intEncoder = create((i: Int) => LongValue.of(i.toLong))
+  implicit val blobEncoder = create(BlobValue.of)
   implicit val booleanEncoder = create(BooleanValue.of)
+  implicit val doubleEncoder = create(DoubleValue.of)
+  implicit val keyEncoder = create(KeyValue.of)
+  implicit val latLngEncoder = create(LatLngValue.of)
+  implicit def seqEncoder[T](implicit ve: ValueEncoder[T]) =
+    create[Seq[T], java.util.List[_ <: Value[_]]](seq =>
+      ListValue.of(seq.map(t => ve.encode(t)).asJava)
+    )
+  implicit def optionEncoder[T] = ???
+  implicit val intEncoder = create((i: Int) => LongValue.of(i.toLong))
+  implicit val longEncoder = create(LongValue.of)
+  implicit val stringEncoder = create(StringValue.of)
+  implicit val timestampEncoder =
+    create((t: java.sql.Timestamp) => TimestampValue.of(Timestamp.of(t)))
 }
 
 trait EntityEncoder[T] extends ValueEncoder[T] {
-  def encodeEntity(t: T): FullEntity[IncompleteKey]
+  def encodeEntity(t: T)(implicit
+      datastore: Datastore
+  ): FullEntity[IncompleteKey]
+
+  def encodeEntity(t: T, keyName: String)(implicit
+      datastore: Datastore
+  ): FullEntity[Key]
 }
 
 object EntityEncoder {
@@ -38,8 +64,8 @@ object EntityEncoder {
     new EntityEncoder[T] {
       type Out = FullEntity[_]
 
-      def encodeEntity(t: T) = {
-        val eb = ctx.parameters.foldLeft(FullEntity.newBuilder()) { (eb, p) =>
+      def encodeEntity[K <: IncompleteKey](t: T, z: FullEntity.Builder[K]) = {
+        val eb = ctx.parameters.foldLeft(z) { (eb, p) =>
           eb.set(
             p.label,
             p.typeclass.encode(p.dereference(t))
@@ -48,8 +74,19 @@ object EntityEncoder {
         eb.build()
       }
 
+      def encodeEntity(t: T)(implicit datastore: Datastore) = {
+        val key = datastore.keyFactory.setKind(ctx.typeName.short).newKey()
+        encodeEntity(t, FullEntity.newBuilder(key))
+      }
+
+      def encodeEntity(t: T, keyName: String)(implicit datastore: Datastore) = {
+        val key =
+          datastore.keyFactory.setKind(ctx.typeName.short).newKey(keyName)
+        encodeEntity(t, FullEntity.newBuilder(key))
+      }
+
       def encode(t: T) = {
-        EntityValue.of(encodeEntity(t))
+        EntityValue.of(encodeEntity(t, FullEntity.newBuilder()))
       }
     }
 
