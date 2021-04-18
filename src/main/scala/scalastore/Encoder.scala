083811878fdad0797d8b5f5977cb30186ef8fdf2
Pishen Tsai <pishen02@gmail.com>
2021-04-18 10:31:56 +0800

    Implement ValueEncoder for Seq and Option

--- src/main/scala/scalastore/Encoder.scala
+++ src/main/scala/scalastore/Encoder.scala
@@ -1,32 +1,17 @@
 package scalastore
 
 import com.google.cloud.Timestamp
-import com.google.cloud.datastore.BlobValue
-import com.google.cloud.datastore.BooleanValue
-import com.google.cloud.datastore.DoubleValue
-import com.google.cloud.datastore.EntityValue
-import com.google.cloud.datastore.FullEntity
-import com.google.cloud.datastore.IncompleteKey
-import com.google.cloud.datastore.Key
-import com.google.cloud.datastore.KeyValue
-import com.google.cloud.datastore.LatLngValue
-import com.google.cloud.datastore.ListValue
-import com.google.cloud.datastore.LongValue
-import com.google.cloud.datastore.StringValue
-import com.google.cloud.datastore.TimestampValue
-import com.google.cloud.datastore.Value
+import com.google.cloud.datastore.{Datastore => _, _}
 import magnolia._
 import scala.jdk.CollectionConverters._
 import scala.language.experimental.macros
 
 trait ValueEncoder[T] {
-  type Out
-  def encode(t: T): Value[Out]
+  def encode(t: T): Value[_]
 }
 
 object ValueEncoder {
-  def create[T, O](f: T => Value[O]) = new ValueEncoder[T] {
-    type Out = O
+  def create[T](f: T => Value[_]) = new ValueEncoder[T] {
     def encode(t: T) = f(t)
   }
 
@@ -36,10 +21,12 @@ object ValueEncoder {
   implicit val keyEncoder = create(KeyValue.of)
   implicit val latLngEncoder = create(LatLngValue.of)
   implicit def seqEncoder[T](implicit ve: ValueEncoder[T]) =
-    create[Seq[T], java.util.List[_ <: Value[_]]](seq =>
-      ListValue.of(seq.map(t => ve.encode(t)).asJava)
-    )
-  implicit def optionEncoder[T] = ???
+    create[Seq[T]](seq => ListValue.of(seq.map(t => ve.encode(t)).asJava))
+  implicit def optionEncoder[T](implicit ve: ValueEncoder[T]) =
+    create[Option[T]] {
+      case Some(t) => ve.encode(t)
+      case None    => NullValue.of()
+    }
   implicit val intEncoder = create((i: Int) => LongValue.of(i.toLong))
   implicit val longEncoder = create(LongValue.of)
   implicit val stringEncoder = create(StringValue.of)
