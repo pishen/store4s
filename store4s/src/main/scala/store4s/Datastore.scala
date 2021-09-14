61ac22620a1c7b7500881f66a6155eb325673f25
syhsiao <shengying.hsiao@vpon.com>
2021-09-14 07:25:09 +0000

    Remove Option from get() result and add getEithers() & getRights()

--- store4s/src/main/scala/store4s/Datastore.scala
+++ store4s/src/main/scala/store4s/Datastore.scala
@@ -23,20 +23,25 @@ case class Datastore(underlying: GDatastore) {
   def get(key: Key) = Option(
     underlying.get(key, Seq.empty[ReadOption]: _*)
   )
-  def get(key: Seq[Key]) = Option(
-    underlying.get(key.asJava, Seq.empty[ReadOption]: _*)
-  )
+  def get(key: Seq[Key]) = underlying.get(key.asJava, Seq.empty[ReadOption]: _*)
   def getEither[A: WeakTypeTag: EntityDecoder](name: String) = {
     get(keyFactory[A].newKey(name)).map(decodeEntity[A])
   }
   def getEither[A: WeakTypeTag: EntityDecoder](id: Long) = {
     get(keyFactory[A].newKey(id)).map(decodeEntity[A])
   }
+  def getEithers[A: WeakTypeTag: EntityDecoder](nameList: Seq[String]) = {
+    get(nameList.map(name => keyFactory[A].newKey(name))).asScala
+      .map(decodeEntity[A])
+  }
   def getRight[A: WeakTypeTag: EntityDecoder](name: String) = {
     getEither[A](name).map(_.toTry.get)
   }
   def getRight[A: WeakTypeTag: EntityDecoder](id: Long) = {
     getEither[A](id).map(_.toTry.get)
+  }
+  def getRights[A: WeakTypeTag: EntityDecoder](name: Seq[String]) = {
+    getEithers[A](name).map(_.toTry.get)
   }
   def delete(key: Key) = underlying.delete(key)
   def update(entity: Entity) = underlying.update(entity)
