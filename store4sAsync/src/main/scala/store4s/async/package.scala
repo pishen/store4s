36694ce651d14c4c1ad422a8e4598fad785a0884
Pishen Tsai <pishen02@gmail.com>
2022-11-09 14:33:37 +0800

    Provide helper functions for boolean Filter

--- store4sAsync/src/main/scala/store4s/async/package.scala
+++ store4sAsync/src/main/scala/store4s/async/package.scala
@@ -1,6 +1,11 @@
 package store4s
 
-import store4s.async.model._
+import store4s.async.model.CompositeFilter
+import store4s.async.model.Filter
+import store4s.async.model.Key
+import store4s.async.model.PartitionId
+import store4s.async.model.PathElement
+import scala.language.implicitConversions
 
 import scala.reflect.runtime.universe._
 
@@ -32,5 +37,18 @@ package object async {
       Some(Key(partitionId, Seq(PathElement(kind, None, Some(name))))),
       Set.empty[String]
     )
+  }
+
+  implicit class FilterWrapper(left: Filter) {
+    def &&(right: Filter) =
+      Filter(compositeFilter = Some(CompositeFilter("AND", Seq(left, right))))
+  }
+
+  implicit class BooleanPropertyWrapper(p: Query.Property[Boolean]) {
+    def unary_! = p == false
+  }
+
+  implicit def booleanProperty2Filter(p: Query.Property[Boolean]): Filter = {
+    p == true
   }
 }
