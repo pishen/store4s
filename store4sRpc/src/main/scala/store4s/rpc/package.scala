2b54d2a9f7fdf531f76782e8eb0e249b030130bb
Pishen Tsai <pishen02@gmail.com>
2024-06-09 23:39:44 +0800

    Prefix implicit class with Store4s to prevent conflict

--- store4sRpc/src/main/scala/store4s/rpc/package.scala
+++ store4sRpc/src/main/scala/store4s/rpc/package.scala
@@ -6,23 +6,23 @@ import com.google.datastore.v1.query.CompositeFilter.Operator.AND
 import com.google.datastore.v1.query.Filter
 
 package object rpc {
-  implicit class EncoderOps[T](t: T)(implicit enc: Encoder[T]) {
+  implicit class Store4sEncoderOps[T](t: T)(implicit enc: Encoder[T]) {
     def asEntity(id: Long): Entity = enc.withId(_ => id).encodeEntity(t)
     def asEntity(name: String): Entity = enc.withName(_ => name).encodeEntity(t)
     def asEntity: Entity = enc.encodeEntity(t)
   }
 
-  implicit class EntityOps(entity: Entity) {
+  implicit class Store4sEntityOps(entity: Entity) {
     def as[T: Decoder]: T = implicitly[Decoder[T]].decodeEntity(entity)
   }
 
-  implicit class FilterWrapper(left: Filter) {
+  implicit class Store4sFilterWrapper(left: Filter) {
     def &&(right: Filter) = Filter().withCompositeFilter(
       CompositeFilter(op = AND).addFilters(left, right)
     )
   }
 
-  implicit class BooleanPropertyWrapper(p: Query.Property[Boolean]) {
+  implicit class Store4sBooleanPropertyWrapper(p: Query.Property[Boolean]) {
     def unary_! = p == false
   }
 
